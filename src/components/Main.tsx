96bf9c93ce8a461d3b4bea6972d903d5c69fda72
Kramarukha Vladislav <Vladislav.Kramarukha@sitronics-kt.ru>
2026-01-20 00:44:01 +0400

    updates

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,5 @@
 import type { SetupContext } from 'vue'
 
 export default function Main(_: {}, { slots }: SetupContext) {
-    return <main class={'flex flex-auto p-2'}>{slots.default ? slots.default() : null}</main>
+    return <main class={'flex grow shrink-0 basis-auto p-2'}>{slots.default ? slots.default() : null}</main>
 }
